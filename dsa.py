aad14d091c3a680d498043b6cf1719d72de427d0
P.Sujay Daniel <p.sujaydaniel@PSujays-MacBook-Air-2.local>
2024-09-26 10:33:16 +0530

    h*h pattern

--- dsa.py
+++ dsa.py
@@ -5,3 +5,14 @@ for i in range(5-1):
   print(a, end =" ")
 for j in range(5):
   print(a)
+
+
+# 5 cross 5 patterns 
+a =5
+
+for i in range(5-1):
+  for j in range(5):
+    print(a,end="")
+  print()  
+
+ 
