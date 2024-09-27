4b721426d85f7d3616fdc2f5ff808386bb82f806
P.Sujay Daniel <p.sujaydaniel@PSujays-MacBook-Air-2.local>
2024-09-27 12:07:39 +0530

    a-z inverted triangle

--- pattern/alpha.py
+++ pattern/alpha.py
@@ -9,4 +9,15 @@ for i in range(a):
   print()
 
 
+#only line a and b 
+
+def numtoChar(num):
+  return chr(num+65)
+
+a = 5
+for i in range(a):
+  for j in range(i+1):
+    print(numtoChar(i),end=" ")
+  print()
+
 
