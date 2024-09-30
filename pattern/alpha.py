da3e90995b6d080c313bc2073a8b1f65248ababe
P.Sujay Daniel <p.sujaydaniel@PSujays-MacBook-Air-2.local>
2024-09-30 22:26:27 +0530

    reverse alphabet triangle

--- pattern/alpha.py
+++ pattern/alpha.py
@@ -21,3 +21,13 @@ for i in range(a):
   print()
 
 
+# reverse triangle of alphabets 
+def numtochar(num):
+  return chr(num+65)
+
+a = 5 
+for i in range(a):
+  for j in range(a-i):
+    print(numtochar(j),end="")
+  print()
+
